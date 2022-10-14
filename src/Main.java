277bc2c2b65027e2b37b93bf6edb799bb70bc5a4
1E10 laptop <sample@bths.edu>
2022-10-14 10:55:14 -0400

    Stuff

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
-public class CircleRunner {
+public class Main {
     public static void main(String[] args){
-        Circle cir1 = new Circle(5.0);
-        System.out.println(cir1.getInfo());
-        cir1.setRadius(9.2);
-        System.out.println(cir1.getInfo());
+//        Circle cir1 = new Circle(5.0);
+//        System.out.println(cir1.getInfo());
+//        cir1.setRadius(9.2);
+//        System.out.println(cir1.getInfo());
+//
+//        Circle cir2 = new Circle(2.6);
+//        System.out.println(cir2.getInfo());
+//        cir2.setRadius(1.2);
+//        System.out.println(cir2.getInfo());
 
-        Circle cir2 = new Circle(2.6);
-        System.out.println(cir2.getInfo());
-        cir2.setRadius(1.2);
-        System.out.println(cir2.getInfo());
+//        RightTriangle r1 = new RightTriangle(3, 4);
+//        System.out.println(r1.hypotenuse());
+//        RightTriangle r2 = new RightTriangle(6.5, 10.7);
+//        System.out.println(r2.hypotenuse());
+
+
+        for (int i = 0; i < 100; i++) {
+            int randomNum = (int) (Math.random() * 51) + 35;
+                    System.out.println(randomNum);
+        }
+
+        }
+
     }
-}
+
